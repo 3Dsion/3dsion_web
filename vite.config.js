2c97d0c586e421176f222798bb35c138af7631d7
Rodrigo <info@3dsion.com>
2026-02-13 02:22:07 -0300

    chore: optimizar base path para desarrollo y producción

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/3dsion_web/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/3dsion_web/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}))
